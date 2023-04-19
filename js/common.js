550810ae33ec27dc6426a85b692c536e782c5238
shusuiqi <shusuiqi@qq.com>
2023-04-19 23:42:26 +0800

    注册与登录

--- js/common.js
+++ js/common.js
@@ -5,3 +5,20 @@ document.addEventListener('DOMContentLoaded', () => {
     document.body.classList.add('ready')
   }, 200)
 })
+
+//--------------------------------------------------
+// bootstrap轻提示
+const toastBox = document.querySelector('#myToast')
+const toast = new bootstrap.Toast(toastBox, {
+  animation: true, // 开启过渡动画
+  autohide: true, // 开启自动隐藏
+  delay: 3000 // 3000ms后自动隐藏
+})
+function tip(msg) {
+  toastBox.querySelector('.toast-body').innerHTML = msg
+  toast.show()
+}
+
+//--------------------------------------------------
+//根路径
+axios.defaults.baseURL = 'http://ajax-api.itheima.net'
